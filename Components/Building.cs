47222a8d0f8793ae476f5f93c995b5de0d870e54
Spageetus <blakehellenbrand@gmail.com>
2025-12-15 22:31:05 -0500

    slowly figuring out how this works

---
+++ Components/Building.cs
@@ -0,0 +1,26 @@
+﻿namespace LayoutPlannerPOC.Components
+{
+    public class Building
+    {
+        public static List<Building> BuildingsList { get; set; }
+        private static int lastId = 0;
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Color { get; set; }
+        public int? Width { get; set; }
+        public int? Length { get; set; }
+
+        public Building(string? Name, string? Color, int? Width, int? Length)
+        {
+            if (Building.BuildingsList is null) BuildingsList = new List<Building>();
+            this.Id = lastId++;
+            this.Name = Name;
+            this.Color = Color;
+            this.Width = Width;
+            this.Length = Length;
+            Building.BuildingsList.Add(this);
+           
+         
+        }
+    }
+}
---
+++ wwwroot/js/scripts.js
@@ -0,0 +1,13 @@
+﻿// In wwwroot/js/myJsFile.js
+
+
+window.someJsFunction = (jsObject) => {
+    console.log("Received object:", jsObject);
+    console.log("Id:", jsObject.id);
+    console.log("Name:", jsObject.name);
+    console.log("Color:", jsObject.color);
+    console.log("Length:", jsObject.length);
+    console.log("Width:", jsObject.width);
+    // Use the object properties as needed
+};
+
