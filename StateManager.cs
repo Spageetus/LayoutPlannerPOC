bfd4c756c579cdf78612f97c082f520328aef495
Spageetus <blakehellenbrand@gmail.com>
2026-03-18 16:31:42 -0400

    the StateManager now remembers if properties have been edited, but never retrieved

--- StateManager.cs
+++ StateManager.cs
@@ -1,13 +1,23 @@
 ﻿using LayoutPlannerPOC.Components;
 using LayoutPlannerPOC.Data;
+using System.Reflection.Metadata;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.Swift;
 
 namespace LayoutPlannerPOC
 {
+    
+
     //sealed means this class cannot be inherited
     public sealed class StateManager //Singleton for storing data used across components
     {
+        struct ChangedState
+        {
+            public const uint PlacedComponents = 1 << 0;
+            public const uint HeldComponent = 1 << 1;
+        }
+
+
         //Lazy initialization of singleton instance
         //src: https://www.codeproject.com/articles/Thread-Safe-Singleton-in-Csharp-A-Guide-to-Double#comments-section
         private static readonly Lazy<StateManager> _instance = new Lazy<StateManager>(() => new StateManager()); //not exactly sure how this is supposed to work
@@ -17,6 +27,13 @@ namespace LayoutPlannerPOC
         private static string _selectedComponentType = "none"; //stores the name of the component that has been selected via hotkeys or component browser.
 
         private static FactoryComponent? heldComponent = null;
+
+        //stores which properties have been altered, but not retrieved
+        private static uint _changedStates = 0;
+
+        public static bool StateHasChanged { get { Console.WriteLine(_changedStates);  return _changedStates != 0; } }
+        //TODO: use the _changedStates byte to store if/what has changed in the StateManager (ex: components have changed, held component has changed, etc).
+        //More accurately, _changedStates tracks which values have been altered, but never retrieved
 
 
         //public accessor for singleton instance
@@ -28,6 +45,9 @@ namespace LayoutPlannerPOC
             if (selectedComponentType == null) selectedComponentType = "none";
             StateManager._selectedComponentType = selectedComponentType;
             StateManager.heldComponent = FactoryComponent.CreateFromName(selectedComponentType);
+
+            //marking the HeldComponent as being changed
+            StateManager._changedStates |= ChangedState.HeldComponent;
         }
 
         public static string GetSelectedComponentType()
@@ -37,6 +57,8 @@ namespace LayoutPlannerPOC
 
         public static FactoryComponent? GetHeldComponent()
         {
+            //marking the held component as not being changed
+            StateManager._changedStates &= ~ChangedState.HeldComponent;
             return heldComponent;
         }
 
@@ -44,24 +66,34 @@ namespace LayoutPlannerPOC
         {
             if (c == null) return;
             _componentsList.Add(c);
+            //marking the placed components as being changed
+            StateManager._changedStates |= ChangedState.PlacedComponents;
         }
 
         public static bool DeleteComponent(FactoryComponent c)
         {
-            return _componentsList.Remove(c);
+            bool removed = _componentsList.Remove(c);
+            //marking the placed components as being changed 
+            if (removed) StateManager._changedStates |= ChangedState.PlacedComponents;
+            return removed;
         }
 
         public static void ClearSelection()
         {
             StateManager._selectedComponentType = "none";
             StateManager.heldComponent = null;
-            
+            //marking held component as being changed
+            StateManager._changedStates |= ChangedState.HeldComponent;
+
         }
 
         public static List<FactoryComponent> GetFactoryComponents()
         {
+            //marking placed components as being unchanged 
+            StateManager._changedStates &= ~ChangedState.PlacedComponents;
             return _componentsList;
         }
+
 
         public static FactoryComponent? GetComponentAtLocation(int x, int y)
         {
